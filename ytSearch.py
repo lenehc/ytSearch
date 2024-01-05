83dd9391c1d80efc7c9eb78c40feef57ff5ead89
lenehc <chenel.nguessan-laurent@outlook.com>
2024-01-05 15:32:09 +0100

    Added help and usage

--- ytSearch.py
+++ ytSearch.py
@@ -6,6 +6,7 @@ import requests
 import sys
 import re
 
+from itertools import zip_longest
 from json import load, JSONDecodeError
 from jsonschema import validate
 from textwrap import wrap, shorten
@@ -15,6 +16,21 @@ from prompt_toolkit.formatted_text import FormattedText
 
 
 INVIDIOUS_INSTANCES = ['yewtu.be', 'vid.puffyan.us', 'yt.artemislena.eu', 'invidious.projectsegfau.lt', 'invidious.slipfox.xyz', 'invidious.privacydev.net', 'vid.priv.au', 'iv.melmac.space', 'iv.ggtyler.dev', 'invidious.lunar.icu', 'inv.zzls.xyz', 'inv.tux.pizza', 'invidious.protokolla.fi', 'iv.nboeck.de', 'invidious.private.coffee', 'yt.drgnz.club', 'iv.datura.network', 'invidious.fdn.fr', 'invidious.perennialte.ch', 'yt.cdaut.de', 'invidious.drgns.space', 'inv.us.projectsegfau.lt', 'invidious.einfachzocken.eu', 'invidious.nerdvpn.de', 'inv.n8pjl.ca', 'youtube.owacon.moe']
+COMMANDS_USAGE = {
+    'search': 'Search for channels and videos sorted by relevance. Arguments: \"search [searchTerm]\", searchTerm is required. Example \"search toosie slide\"', 
+    'channel': 'List all videos from a channel sorted by date published. Arguments: \"channel [channelId]\", channelId is required. Example \"channel UCByOQJjav0CUDwxCk-jVNRQ\"'
+}
+CONFIG_VAR_HELP = {
+    'invidiousInstance': 'The number corresponding to the invidious instance that you want to use, see the table above for a list of available instances. Example: 2',
+    'blockedVideoTitles': 'A dictionary of regex patterns, any video whose title matches a pattern will not be shown. To mark the filter as case-insensitive add a value of \"true\", else \"false\". Example: { \"toosie slide\": false }',
+    'blockedChannelNames': 'A dictionary of regex patterns, any channel whose names matches a pattern will not be shown. To mark the filter as case-insensitive add a value of \"true\", else \"false\". Example: { \"drake\": true }',
+    'blockedChannelIds': 'A list of strings, any channel whose ID matches a string will not be shown. Example: [ \"UCByOQJjav0CUDwxCk-jVNRQ\" ]',
+    'ui.margin': 'The number of spaces to prefix each line of text with. Example: 3',
+    'ui.width': 'The maxiumum number of columns that a line should be before being wrapped. Example: 50',
+    'ui.searchResultsLimit': 'The maximum number of search results (videos and channels) to show. Example: 10',
+    'ui.channelVideosLimit': 'The maximum number of a channel\'s videos to show (when using command \"channel\"). Example: 500',
+    'ui.textColor.gray1, ui.textColor.gray2, ui.textColor.gray3': 'Hex color codes in string format used to color the interface. Example: \"#8e8e93\"'
+}
 
 
 class Config:
@@ -157,15 +173,46 @@ def printError(error):
     printLn([('gray1', error)])
 
 
-def printUsage():
+def printTable(table, fieldWidth=15, fieldValueWidth=40, newLineBetweenRows=False):
+    formatRow = lambda key, value: '{}{}    {}'.format(MARGIN, str(key).rjust(fieldWidth), value)
+
+    for idx, (key, value) in enumerate(table.items()):
+        for fieldRow, valueRow in zip_longest(wrap(str(key), width=fieldWidth), wrap(str(value), width=fieldValueWidth), fillvalue=''):
+            printLn([('gray1', formatRow(fieldRow, valueRow))])
+        if newLineBetweenRows and idx != len(table)-1:
+            print()
+
+
+def printUsage(command=None, standAlone=False):
+    if standAlone:
+        print()
+
+    printLn([('gray1', 'Usage')])
     print()
-    printLn([('gray1', 'Usage       search [term]')])
-    printLn([('gray1', '            channel [id]')])
+
+    if command:
+        printTable({command: COMMANDS_USAGE[command]}, fieldWidth=8)
+    else:
+        printTable(COMMANDS_USAGE, fieldWidth=8, newLineBetweenRows=True)
+
+    if standAlone:
+        printRule()
+
+
+def printHelp():
+    print()
+    printUsage()
     printRule()
     printLn([('gray1', 'Instances')])
     print()
+
     for idx, instance in enumerate(INVIDIOUS_INSTANCES):
-        printLn([('gray1', '  {} {}'.format(str(idx+1).rjust(2).ljust(4), instance))])
+        printLn([('gray1', '  {}   {}'.format(str(idx+1).rjust(2), instance))])
+
+    printRule()
+    printLn([('gray1', 'Config')])
+    print()
+    printTable(CONFIG_VAR_HELP, fieldWidth=24, newLineBetweenRows=True)
     printRule()
 
 
@@ -201,6 +248,16 @@ def get(destDict, key, defaultValue=''):
     if value == None:
         return defaultValue
     return value
+
+
+def shortenString(string, width, suffix=''):
+    width = width - len(suffix)
+    string = string.strip()
+    if width <= 0:
+        return '…{}'.format(suffix)
+    if len(string) > width:
+        return '{}…{}'.format(string[:width-1].rstrip(), suffix)
+    return '{}{}'.format(string, suffix)
 
 
 class Channel:
@@ -241,13 +298,9 @@ class Video:
     def videoInfo(self):
         meta = '{} · {}'.format(self.viewCountText, self.publishedText)
         channelNameWidth = WIDTH-len(meta)-4
+        channelName = shortenString(self.channelName, width=channelNameWidth, suffix=self.channelVerifiedText)
 
-        if channelNameWidth > 0:
-            channelName = shorten(self.channelName, width=channelNameWidth, placeholder='…')
-        else:
-            channelName = '…'
-
-        return '{}    {}'.format('{}{}'.format(channelName, self.channelVerifiedText).ljust(channelNameWidth), meta)
+        return '{}    {}'.format(channelName.ljust(channelNameWidth), meta)
 
     def render(self):
         blocks = wrap(self.videoTitle, width=WIDTH, max_lines=2, placeholder='…')
@@ -390,26 +443,35 @@ class App:
         
         self.renderResults(videos, noResultsText='')
     
+    def run(self, command, params):
+        if command == 'search':
+            if params:
+                self.search(params)
+                return True
+            printUsage(command, standAlone=True)
+            sys.exit(1)
+        elif command == 'channel':
+            if params:
+                if len(params) > 1:
+                    printError('Unrecognized argument \"{}\"'.format(params[1]))
+                    sys.exit(1)
+                self.listChannelVideos(params[0])
+                return True
+            printUsage(command, standAlone=True)
+            sys.exit(1)
+
+        return False
+    
 
 def main():
     args = sys.argv[1:]
 
-    if len(args) < 2:
-        printUsage()
-        sys.exit(1)
+    if args:
+        command, *params = args
+        if App().run(command, params):
+            sys.exit(0)
 
-    command, *params = args
-
-    app = App()
-
-    if command == 'search':
-        app.search(params)
-        sys.exit(0)
-    elif command == 'channel':
-        app.listChannelVideos(params[0])
-        sys.exit(0)
-
-    printUsage()
+    printHelp()
     sys.exit(1)
 
 
